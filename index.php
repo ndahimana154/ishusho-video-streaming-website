0b3d83255d677ae838e5e23dc05983e9c53fd4dc
ndahimana154 <ndahimana154@gmail.com>
2023-11-02 11:23:33 +0200

    The watch movie page is working but it remains with the quality switching and the Workings Views count ass well as the shares and feedbacks

--- index.php
+++ index.php
@@ -17,26 +17,9 @@ include('./php/global/server.php');
 </head>
 
 <body>
-  <!-- Navbar -->
-  <nav class="navbar navbar-expand-lg bg-dark">
-    <div class="container-fluid">
-      <a class="navbar-brand text-light fs-3 fw-bold me-auto" href="#">IS<span class="text-danger">HUS</span>HO</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <form class="d-flex mx-auto w-50" role="search">
-          <input class="form-control border border-success bg-transparent text-light custom-placeholder" type="search" placeholder="Type to search..." aria-label="Search">
-          <button class="btn btn-outline-success ms-2" type="submit">
-            <i class="fas fa-search"></i>
-          </button>
-        </form>
-        <div class="">
-        </div>
-      </div>
-    </div>
-  </nav>
-  <!-- Navbar -->
+  <?php
+  include("./php/client/nav-bar.php")
+  ?>
 
   <!-- Hero -->
   <!-- <div id="carouselExampleCaptions" class="carousel  container slide mt-2" data-bs-ride="carousel">
---
+++ watch.php
@@ -0,0 +1,163 @@
+<?php
+session_start();
+include('./php/global/server.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/bootstrap-5.0.2-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/fontawesome-free-6.4.2-web/css/all.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- Include Plyr CSS -->
+    <link rel="stylesheet" href="https://cdn.plyr.io/3.6.2/plyr.css" />
+
+
+    <link rel="stylesheet" href="./styles/main.css">
+
+    <title>ISHUSHO MOVIES</title>
+</head>
+
+<body>
+    <?php
+    include("./php/client/nav-bar.php");
+    if (!isset($_GET['v'])) {
+    ?>
+        <p class="alert alert-danger">
+            No video sent to server.
+        </p>
+        <?php
+    } else {
+        $video = $_GET['v'];
+        // Check the video existence
+        $checkVideoExistence = mysqli_query($server, "SELECT * from
+            movies WHERE movie_id = '$video'
+        ");
+        if (mysqli_num_rows($checkVideoExistence) != 1) {
+        ?>
+            <p class="alert alert-danger">
+                404 :) Movie is not found
+            </p>
+        <?php
+        } else {
+            $dataVideoExistence = mysqli_fetch_array($checkVideoExistence);
+            $youtubeLink = $dataVideoExistence['movie_url'];
+            // Extract the video ID from the link
+            $videoId = '';
+            $parsedUrl = parse_url($youtubeLink);
+            if (isset($parsedUrl['query'])) {
+                parse_str($parsedUrl['query'], $query);
+                if (isset($query['v'])) {
+                    $videoId = $query['v'];
+                }
+            }
+
+        ?>
+            <div class="center-section">
+                <div class="left-video">
+                    <div class="video-display">
+                        <div id="player">
+                            <iframe src="https://www.youtube.com/embed/<?php echo $videoId ?>?si=uVoE1p9JnySMhtTF" class="movie_displayer" poster="<?php echo $dataVideoExistence['movie_poster']; ?>" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                        </div>
+                        <div class="video-controls">
+                            <h2>
+                                <?php echo $dataVideoExistence['movie_name']; ?>
+                            </h2>
+                            <div class="ctrlz">
+                                <a>
+                                    <i class="fa fa-eye"></i> <span>100</span>
+                                </a>
+
+                                <a>
+                                    <i class="fa fa-thumbs-up"></i>
+                                    <span>30</span>
+                                </a>
+
+                                <a>
+                                    <i class="fa fa-comment"></i>
+                                    <span>10</span>
+                                </a>
+                            </div>
+
+
+                        </div>
+                        <div class="description">
+                            <h4>
+                                Overview:
+                            </h4>
+                            <?php
+                            echo $dataVideoExistence['movie_description'];
+                            ?>
+                        </div>
+                    </div>
+                </div>
+                <div class="recommended-vids">
+                    <h1>
+                        Other recommendations
+                    </h1>
+                    <?php
+                    $getRecommendedOnes = mysqli_query($server, "SELECT * from movies
+                            WHERE movie_id != '$video'
+                            ORDER BY rand()    
+                            LIMIT 8                 
+                        ");
+                    while ($dataRecommendationsVideos = mysqli_fetch_array($getRecommendedOnes)) {
+                    ?>
+                        <div class="recommend-box">
+                            <a href="watch.php?v=<?php echo $dataRecommendationsVideos['movie_id'] ?>"
+                                class="recommend">
+                                <div class="left">
+                                    <img src="<?php echo $dataRecommendationsVideos['movie_poster']; ?>" alt="">
+                                </div>
+                                <div class="right">
+                                    <span class="m_name">
+                                        <?php
+                                        echo $dataRecommendationsVideos['movie_name'];
+                                        ?>
+                                    </span>
+
+                                    <p title="<?php echo $dataRecommendationsVideos['movie_description']; ?>">
+                                        <!-- <i class="fa fa-film"></i> -->
+                                        <?php echo $dataRecommendationsVideos['movie_description']; ?>
+                                    </p>
+                                    <div class="buttons">
+                                        <button>
+                                            <i class="fa fa-eye"></i>
+                                            <span>100</span>
+                                        </button>
+                                        <button>
+                                            <i class="fa fa-thumbs-up"></i>
+                                            <span>100</span>
+                                        </button>
+                                        <button>
+                                            <i class="fa fa-comment"></i>
+                                            <span>100</span>
+                                        </button>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    <?php
+                    }
+                    ?>
+                </div>
+            </div>
+    <?php
+        }
+    }
+    ?>
+
+    <!-- End latest movies -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- Include Plyr JS -->
+    <script src="https://cdn.plyr.io/3.6.2/plyr.js"></script>
+
+    <script>
+        const player = new Plyr('#player');
+    </script>
+</body>
+
+</html>
